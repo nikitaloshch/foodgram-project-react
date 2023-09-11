e0b280a6eb497d963ca3adaa0ebe4469756c793f
Никита Лощинин <terasentplay@yandex.ru>
2023-09-11 23:10:05 +0700

    fixes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+    'colorfield',
     'debug_toolbar',
     'recipes.apps.RecipesConfig',
     'users.apps.UsersConfig',
