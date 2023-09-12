040112fab65ac0ccaa829075d0fb2e9133620f49
Никита Лощинин <terasentplay@yandex.ru>
2023-09-13 02:45:36 +0700

    fixes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='any_string')
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
