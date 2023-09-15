b4831646b69cef9feadb51f8f23765c708d33571
Никита Лощинин <terasentplay@yandex.ru>
2023-09-15 18:57:56 +0700

    fixes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views.recipes import (
-    IngredientViewSet, RecipeViewSet, TagViewSet, ShoppingCartViewSet
-)
+from .views.recipes import (IngredientViewSet, RecipeViewSet,
+                            TagViewSet, ShoppingCartViewSet)
 from .views.users import CustomUserViewSet, SubscriptionViewSet
+
+app_name = 'api'
 
 router = routers.DefaultRouter()
 router.register('users', CustomUserViewSet, basename='users')
